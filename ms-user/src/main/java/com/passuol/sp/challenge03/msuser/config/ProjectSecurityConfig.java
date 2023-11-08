ddb452f411fd03191afe9e34a2bed02ff106c6d2
ThiagoMdO <128644651+ThiagoMdO@users.noreply.github.com>
2023-11-08 14:13:40 -0300

    feat(wip):for correction with Lucas Magno

--- ms-user/src/main/java/com/passuol/sp/challenge03/msuser/config/ProjectSecurityConfig.java
+++ ms-user/src/main/java/com/passuol/sp/challenge03/msuser/config/ProjectSecurityConfig.java
@@ -4,7 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -22,5 +31,27 @@ public class ProjectSecurityConfig {
         http.csrf(csrf -> csrf.ignoringRequestMatchers("/v1/users", "/v1/login","/v1/users/{id}","/v1/users/{id}/password"));
 
         return http.build();
+    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//        .username("admin")
+//        .password("12345")
+//        .authorities("admin")
+//        .build();
+//
+//    return new InMemoryUserDetailsManager((admin));
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
